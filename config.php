7cd59dd8d2512268d98bfcccd175ccffd071dec9
EvrenIvgen <62255614+EvrenIvgen@users.noreply.github.com>
2020-04-24 10:39:07 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 $ php bin/composer install
 $ ./vendor/bin/doctrine orm:generate-proxies
-$ ./vendor/bin/doctrine orm:schema-tool:create
+$ ./vendor/bin/doctrine orm:schema-tool:create --dump-sql >> schema.sql
